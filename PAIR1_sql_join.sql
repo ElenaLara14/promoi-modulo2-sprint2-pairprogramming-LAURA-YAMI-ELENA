faff190db1c655a049fc7347e33a6147ee2657b0
laural87 <laura.lpzvgs@gmail.com>
2024-07-08 10:41:05 +0200

    ejercicio sql join

--- PAIR1_sql_join.sql
+++ PAIR1_sql_join.sql
@@ -19,4 +19,13 @@ SELECT customers.customer_id, customers.company_name, COUNT(orders.order_id) AS 
     GROUP BY customers.company_name, YEAR(orders.order_date);
     
     
+    -- Lo siguiente que nos han pedido es la misma consulta anterior pero con la adición de la cantidad de dinero que han pedido por esa cantidad de objetos, teniendo en cuenta los descuentos, etc. Ojo que los descuentos en nuestra tabla nos salen en porcentajes, 15% nos sale como 0.15.
+    
+	SELECT customers.company_name, YEAR(orders.order_date) AS anio, SUM(order_details.quantity) AS NumObjetos, SUM((order_details.unit_price * order_details.quantity)*(1-order_details.discount)) AS DineroTotal
+    FROM customers
+    INNER JOIN orders
+    ON customers.customer_id = orders.customer_id
+    INNER JOIN order_details
+    ON orders.order_id = order_details.order_id
+    GROUP BY customers.company_name, YEAR(orders.order_date);
     
